d32b844ef5f4a60bd7c5aba4790144c60579ad25
Arie <ariearfn@gmail.com>
2020-11-21 10:55:11 +0700

    crud member

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -10,6 +10,15 @@ class Member extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'phone_number', 'status'];
+    protected $appends = ['status_label'];
+
+    public function getStatusLabelAttribute()
+    {
+        if ($this->status == 0){
+            return '<span class="text-red-500">Free</span>';
+        }
+        return '<span class="text-teal-500">Pro</span>';
+    }
 }
 
 
